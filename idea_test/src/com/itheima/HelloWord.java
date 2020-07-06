5997668740f786388d3b3a8ed29462afc1650e38
chengxiang <779346051@qq.com>
2020-07-06 11:46:47 +0800

    第二次提交测试2

--- idea_test/src/com/itheima/HelloWord.java
+++ idea_test/src/com/itheima/HelloWord.java
@@ -4,6 +4,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
         System.out.println("helloWorld");
+        System.out.println("helloWorld");
 
     }
 }
