7b550a05d17722a9109272c85b9c15163ea61fc7
chengxiang <779346051@qq.com>
2020-07-06 11:45:41 +0800

    第一次提交测试1

--- idea_test/src/com/itheima/HelloWord.java
+++ idea_test/src/com/itheima/HelloWord.java
@@ -3,5 +3,7 @@ package com.itheima;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
+        System.out.println("helloWorld");
+
     }
 }
